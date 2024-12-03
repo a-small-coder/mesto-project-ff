41a122bc264ea386bf0c0832f5507e532736ea5d
a-small-coder <hulktv222@mail.ru>
2024-12-04 00:18:56 +0300

    ✨ tuned webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import './styles/pages/index.css';
+
 // @todo: Темплейт карточки
 const template = document.getElementById("card-template");
 
+console.log('Hello, World!')
+const numbers = [2, 3, 5];
+
+// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10
 // @todo: DOM узлы
 const placesList = document.querySelector(".places__list");
 
