a55b78ac97548ad966f9295839f07d8a6d20f9fe
a-small-coder <hulktv222@mail.ru>
2024-12-21 03:46:44 +0300

    :bug: fix images imports and run prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import './styles/pages/index.css';
+import "./styles/pages/index.css";
+
+import { initialCards } from "./scripts/cards";
 
 // @todo: Темплейт карточки
 const template = document.getElementById("card-template").content;
 
-console.log('Hello, World!')
+console.log("Hello, World!");
 const numbers = [2, 3, 5];
 
 // Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
+const doubledNumbers = numbers.map((number) => number * 2);
 
 console.log(doubledNumbers); // 4, 6, 10
 // @todo: DOM узлы
